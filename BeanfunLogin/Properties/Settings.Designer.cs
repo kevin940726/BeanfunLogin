11a22840f59dd9c72e59b9586da3cd8d16f46547
葉家郡 <s000032001@gmail.com>
2016-08-02 15:35:35 +0800

    require administrator [testing]

--- BeanfunLogin/Properties/Settings.Designer.cs
+++ BeanfunLogin/Properties/Settings.Designer.cs
@@ -157,7 +157,7 @@ namespace BeanfunLogin.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("190")]
+        [global::System.Configuration.DefaultSettingValueAttribute("191")]
         public int currentVersion {
             get {
                 return ((int)(this["currentVersion"]));
